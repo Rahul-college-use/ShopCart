9544a16f8e56da2ba02faf314d9cf88257f3b640
RAHUL KUMAR <rahulcollegeuse@gmail.com>
2024-11-02 22:54:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+let item_name = document.querySelector('#item_name');
+let price = document.querySelector('#price');
+let addBtn = document.querySelector('#add');
+let clearBtn = document.querySelector('#clear');
+let add_item_box = document.querySelector('.add_items');
+let add_item = document.querySelector('#add_items');
+let total_price = document.querySelector('#total_price');
+let sum = 0
+// console.log(add_item); // Check if add_item is not null
+
+addBtn.addEventListener('click', () => {
+
+    if (item_name.value != '' && price.value != '') {
+        let newDiv = document.createElement('ul');
+        add_item_box.style.display = 'block'
+        newDiv.style.margin = '1vmax';
+        newDiv.innerText = item_name.value;
+
+        // Insert newDiv before add_item if add_item exists
+        if (add_item && add_item.parentNode) {
+            add_item.parentNode.insertBefore(newDiv, add_item);
+        } else {
+            console.error("add_item or its parentNode is not found.");
+        }
+
+
+
+        sum += parseInt(price.value); // Handle any invalid price input as 0
+        total_price.innerText = sum;
+        item_name.value = ''
+        price.value = ''
+
+        let clear = () => {
+            console.log("clear btn")
+            // Clear functionality goes here
+            newDiv.innerText = ''
+            add_item_box.style.display = 'none'
+            sum = 0
+            total_price.innerText = 0
+        };
+
+
+        clearBtn.addEventListener('click', clear);
+
+    }
+
+
+    else {
+        alert("Please fill in both item name and price.");
+
+    }
+
+
+});
+
+
+
